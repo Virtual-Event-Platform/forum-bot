e0510415120c28ef97ae6665099fcc4a4db6cdd9
Gaetan Jonathan <gaetan.s118@gmail.com>
2023-06-04 16:11:32 +0300

    make chat singleton

--- injection.py
+++ injection.py
@@ -1,5 +1,6 @@
-from ampalibe import Messenger, Model
+from base import singleton
 from data.datasources.api import Api
+from ampalibe import Messenger, Model
 from conf import Configuration as config
 from data.datasources.db import CustomModel
 from data.datasources.kavio_data import KavioDataSource
@@ -9,7 +10,10 @@ from domains.usescase.get_assets import AssetUseCase
 from domains.usescase.get_stands import StandUseCase
 from domains.usescase.get_fiches import FicheMetierUseCase
 
-chat, query = Messenger(), Model()
+
+chat = singleton(Messenger)()
+query = singleton(Model)()
+
 statUsecase = StatUseCase(Api(config.PB_URL, config.PB_USER, config.PB_PASSWORD))
 standUseCase = StandUseCase(Api(config.PB_URL, config.PB_USER, config.PB_PASSWORD))
 assetUseCase = AssetUseCase(Api(config.PB_URL, config.PB_USER, config.PB_PASSWORD))
